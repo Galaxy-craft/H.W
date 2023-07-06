d5ff9a0107a2183ae26299ea55c116483e05d76c
Galaxy-craft <116378367+Galaxy-craft@users.noreply.github.com>
2023-07-06 22:09:22 +0500

    .

--- 4/ex2.py
+++ 4/ex2.py
@@ -1,12 +1,13 @@
 numbers = [1, 1]
 a = 0
-num = int(input('Введите какое число вас интересует?'))
-try:
-    print(numbers[num])
-except:
-    while a < num:
-        calc = numbers[a] + numbers[a + 1]
-        numbers.append(calc)
-        print(numbers)
-        a += 1
-    print(numbers[num - 1])
+while True:
+    num = int(input('Введите какое число вас интересует?'))
+    try:
+        print(numbers[num])
+    except:
+        while a < num - 2:
+            calc = numbers[a] + numbers[a + 1]
+            numbers.append(calc)
+            print(numbers)
+            a += 1
+        print(numbers[num - 1])
